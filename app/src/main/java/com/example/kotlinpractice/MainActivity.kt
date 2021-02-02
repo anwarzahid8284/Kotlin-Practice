31718a1c10c9d9f4ea84e3fb08d4f5659402c1a4
Anwar zahid <anwarzahid.bcs@gmail.com>
2021-02-02 23:11:30 +0500

    Kotlin Practice

--- app/src/main/java/com/example/kotlinpractice/MainActivity.kt
+++ app/src/main/java/com/example/kotlinpractice/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinpractice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,18 +9,18 @@ import android.widget.EditText
 import android.widget.TextView
 import java.util.*
 
-class MainActivity : AppCompatActivity() ,View.OnClickListener{
+class MainActivity : AppCompatActivity(){
     lateinit var editText: EditText
     lateinit var textView: TextView
     lateinit var button: Button
+    lateinit var buttonNextActivity: Button;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         editText = findViewById(R.id.inputID)
         textView = findViewById(R.id.showID)
         button = findViewById(R.id.btnShowID)
-        button.setOnClickListener(this)
-        textView.setOnClickListener(this)
+        buttonNextActivity=findViewById(R.id.nextactivitybtnID)
         // second click listener
        /* val clickListener=object :View.OnClickListener{
             override fun onClick(v: View?) {
@@ -42,17 +43,21 @@ class MainActivity : AppCompatActivity() ,View.OnClickListener{
 
 
         // Recommended click listener
-        /*button.setOnClickListener {
+        button.setOnClickListener {
             val result = editText.text
             textView.text = "Hello input $result"
         }
         textView.setOnClickListener {
             val result = editText.text
             textView.text = "Hello View $result"
-        }*/
+        }
+        buttonNextActivity.setOnClickListener{
+            val intent:Intent= Intent(applicationContext,SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 
-    override fun onClick(v: View?) {
+    /*override fun onClick(v: View?) {
         when(v?.id){
             R.id.showID->{
                 val result = editText.text
@@ -63,7 +68,7 @@ class MainActivity : AppCompatActivity() ,View.OnClickListener{
                 textView.text = "Hello View $result"
             }
         }
-    }
+    }*/
 
     /*override fun onClick(v: View?) {
         val result=editText.text
